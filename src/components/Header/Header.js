bcfe4d2cb304615790213f8531447cfbf9eff823
AristoteLeDaron <aristotenataf@gmail.com>
2023-10-11 19:58:10 +0200

    creation du header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,13 @@ const Header = () => {
   return (
     <header className="header">
       <img
-        src={`${process.env.PUBLIC_URL}/logo/sellMeDreamsLogo.png`}
+        src={`${process.env.PUBLIC_URL}/logo/sellMeDreamsLogo.jpg`}
+        alt="Sell Me Dreams Logo"
+        className="logo"
+      />
+      <h1 className="header-title">Et si nos rêves étaient à vendre...</h1>
+      <img
+        src={`${process.env.PUBLIC_URL}/logo/sellMeDreamsLogo.jpg`}
         alt="Sell Me Dreams Logo"
         className="logo"
       />
