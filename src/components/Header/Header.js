a7e2a644d3ee1f3f663a80abf01954d7bf07277a
AristoteLeDaron <aristotenataf@gmail.com>
2023-10-13 19:50:44 +0200

    correction Header.css et NavBar.css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,18 +5,19 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-
-      <img
-        src={`${process.env.PUBLIC_URL}/logo/sellMeDreamsLogo.jpg`}
-        alt="Sell Me Dreams Logo"
-        className="logo"
-      />
-      <h1 className="header-title">Et si nos rêves étaient à vendre...</h1>
-      <img
-        src={`${process.env.PUBLIC_URL}/logo/sellMeDreamsLogo.jpg`}
-        alt="Sell Me Dreams Logo"
-        className="logo"
-      />
+      <div className="top-section">
+        <img
+          src={`${process.env.PUBLIC_URL}/logo/sellMeDreamsLogo.jpg`}
+          alt="Sell Me Dreams Logo"
+          className="logo"
+        />
+        <h1 className="header-title">Et si nos rêves étaient à vendre...</h1>
+        <img
+          src={`${process.env.PUBLIC_URL}/logo/sellMeDreamsLogo.jpg`}
+          alt="Sell Me Dreams Logo"
+          className="logo"
+        />
+      </div>
       <NavBar />
     </header>
   );
